ad834dfaab28ee9049092980664bc6f40813d32c
Gantulga0722 <mgtulga22@gmail.com>
2024-02-19 12:45:12 +0800

    from Pinecone

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { products, users } = require("./dummy.json");
+const app = express();
+
+app.get("/products", (req, res) => {
+  res.type = "application/json";
+  res.send({ products: products });
+});
+app.get("/users", (req, res) => {
+  res.type = "application/json";
+  res.send({ users: users });
+});
+app.get("/usernames", (req, res) => {
+  res.type = "application/json";
+  res.send({ username: users.map((a) => a.name) });
+});
+
+app.listen(3001, () => {
+  console.log("Server is listening at port 3001");
+});
