aade4c21e66133b472eef9bc949ef1591d7c4f1b
Gantulga0722 <mgtulga22@gmail.com>
2024-02-20 12:06:34 +0800

    from Home

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const { products, users } = require("./dummy.json");
+
 const app = express();
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const fs = require("fs");
+const { error } = require("console");
 
 app.get("/products", (req, res) => {
   res.type = "application/json";
@@ -13,6 +20,43 @@ app.get("/users", (req, res) => {
 app.get("/usernames", (req, res) => {
   res.type = "application/json";
   res.send({ username: users.map((user) => user.name) });
+});
+
+app.get("/read-new-user", (req, res) => {
+  fs.readFile("dummyTest.json", (error, data) => {
+    if (error) {
+      console.log("Error in reading");
+    } else {
+      console.log("Data from file", JSON.parse(data.toString()));
+      res.status(200);
+      res.send(data);
+    }
+  });
+});
+
+app.post("/add-user", (req, res) => {
+  const newUser = req.body;
+
+  fs.readFile("dummy.json", (error, data) => {
+    console.log(data);
+    if (error) {
+      console.log("Error in reading file");
+    } else {
+      const jsonFile = JSON.parse(data.toString());
+      jsonFile.users.push(newUser);
+      fs.writeFile("dummy.json", JSON.stringify(jsonFile), (err) => {
+        if (err) {
+          console.log(err);
+          res.send("error happened");
+        } else {
+          console.log("success");
+          res.send("User added successfully");
+        }
+      });
+    }
+  });
+  res.status(200);
+  res.send("User added successfully");
 });
 
 app.listen(3001, () => {
